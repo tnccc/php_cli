f13ec66de3bb9b9d3a502cf713f62c921801b853
tnccc <irei.frontend@gmail.com>
2023-07-18 22:17:50 +0900

    feat: エネミークラスプライベート化

--- classes/enemy.php
+++ classes/enemy.php
@@ -2,14 +2,34 @@
 class Enemy
 {
   const MAX_HITPOINT = 50; // 最大HP
-  public $name; // 人間の名前
-  public $hitPoint = 50; // 現在のHP
-  public $attackPoint = 10; // 攻撃力
+  private $name; // 人間の名前
+  private $hitPoint = 50; // 現在のHP
+  private $attackPoint = 10; // 攻撃力
+
+  public function __construct($name)
+  {
+    return $this->name = $name;
+  }
+
+  public function getName()
+  {
+    return $this->name;
+  }
+
+  public function getHitPoint()
+  {
+    return $this->hitPoint;
+  }
+
+  public function getAttackPoint()
+  {
+    return $this->attackPoint;
+  }
 
   public function doAttack($human)
     {
-      echo "『" .$this->name . "』の攻撃!\n";
-      echo "【" .$human->name."】に" .$this->attackPoint."のダメージ!\n";
+      echo "『" .$this->getName() . "』の攻撃!\n";
+      echo "【" .$human->getName()."】に" .$this->attackPoint."のダメージ!\n";
       $human->tookDamage($this->attackPoint);
     }
 
